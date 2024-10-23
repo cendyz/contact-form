8a9cd819e09e83902ebf23af5626800628158c8a
cendyz <arek57823@gmail.com>
2024-10-23 20:40:43 +0200

    some java

--- src/main.js
+++ src/main.js
@@ -1,12 +1,47 @@
-const navBtn = document.querySelector(".nav__btn-img");
-const navBtnX = document.querySelector(".nav__btn-img--x");
-const navMenu = document.querySelector(".nav__menu");
-const shadow = document.querySelector(".shadow");
+const nameInput = document.querySelector("#name");
+const surnameInput = document.querySelector("#surname");
+const emailInput = document.querySelector("#email");
+const radioOneInput = document.querySelector("#radio-1");
+const radioTwoInput = document.querySelector("#radio-2");
+const inputs = [nameInput, surnameInput, emailInput];
+const errorText = document.querySelectorAll(".main__form-wrapper-error");
+const btn = document.querySelector(".main__form-btn");
+const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
 
-const handleMenu = () => {
-	navBtn.classList.toggle("display-disable");
-	navBtnX.classList.toggle("display-active");
-	shadow.classList.toggle("opacity");
-	navMenu.classList.toggle("menu-active");
-	document.body.classList.toggle("overflow-hidden");
+const setError = (input, index, message) => {
+	input.style.border = "1px solid hsl(0, 66%, 54%)";
+	errorText[index].style.display = "block";
+	errorText[index].textContent = message;
+};
+
+const boxError = (input, index, message) => {
+	errorText[index].style.display = "block";
+	errorText[index].textContent = message;
+};
+
+const clearError = (input, index) => {
+	errorText[index].style.display = "none";
+	input.style.border = "1px solid hsl(186, 15%, 59%)";
+};
+
+const checkInputs = (input, index) => {
+	if (input.value === "" || input.value.length < 4) {
+		setError(input, index, "This field is required");
+	} else if (input.id === 'email' && !emailRegex.test(emailInput.value)) {
+		setError(input, index, "Please enter a valid email address");
+	} else {
+		clearError(input, index);
+	}
+};
+
+btn.addEventListener("click", () => {
+	event.preventDefault();
+	inputs.forEach((input, index) => checkInputs(input, index))
+});
+
+window.onload = () => {
+	inputs.forEach(input => {
+		input.checked = false;
+		input.value = "";
+	});
 };
